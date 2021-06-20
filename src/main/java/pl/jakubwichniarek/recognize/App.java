7935beff2ae20a10b1603df437d68b0317a27b8f
Jakub Wichniarek <jakubwichniarek@yahoo.pl>
2021-06-20 13:56:15 +0200

    first commit to create main branch

---
+++ src/main/java/pl/jakubwichniarek/recognize/App.java
@@ -0,0 +1,13 @@
+package pl.jakubwichniarek.recognize;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
